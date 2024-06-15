d6d2a3390ff7b419ca3b9e6c627e6b253df4339c
yohannd1 <mitonanan12@gmail.com>
2024-06-15 16:59:40 -0300

    minimally functional demo

--- src/main.c
+++ src/main.c
@@ -1,59 +1,54 @@
 #include <gtk/gtk.h>
-#include <glib/gstdio.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
-}
+static void cb_activate(GtkApplication *app, gpointer data);
+static void cb_print_hello(GtkWidget *wid, gpointer data);
+static void cb_quit(GtkWindow *win);
 
-static void
-quit_cb (GtkWindow *window)
-{
-  gtk_window_close (window);
-}
-
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
-{
-  /* Construct a GtkBuilder instance and load our UI description */
-  GtkBuilder *builder = gtk_builder_new ();
-  gtk_builder_add_from_file(builder, "src/builder.ui", NULL);
-
-  /* Connect signal handlers to the constructed widgets. */
-  GObject *window = gtk_builder_get_object (builder, "window");
-  gtk_window_set_application (GTK_WINDOW (window), app);
-
-  GObject *button = gtk_builder_get_object (builder, "button1");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-  button = gtk_builder_get_object (builder, "button2");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-  button = gtk_builder_get_object (builder, "quit");
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (quit_cb), window);
-
-  gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
-
-  /* We do not need the builder any more */
-  g_object_unref (builder);
-}
-
-int
-main (int   argc,
-      char *argv[])
-{
+int main(int argc, char **argv) {
 #ifdef GTK_SRCDIR
-  g_chdir (GTK_SRCDIR);
+	g_chdir (GTK_SRCDIR);
 #endif
 
-  GtkApplication *app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+	GtkApplication *app = gtk_application_new("lp2.editor_texto", G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect(app, "activate", G_CALLBACK(cb_activate), NULL);
 
-  int status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+	int status = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
 
-  return status;
+	return status;
+}
+
+static void cb_activate(GtkApplication *app, gpointer user_data) {
+	// criar a janela
+	GtkWidget *window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "Editor");
+	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+
+	GtkWidget *grid = gtk_grid_new();
+	gtk_window_set_child(GTK_WINDOW(window), grid);
+
+	// hello world
+	GtkWidget *btn_hello = gtk_button_new_with_label("Say hello");
+	g_signal_connect(btn_hello, "clicked", G_CALLBACK(cb_print_hello), NULL);
+	gtk_grid_attach(GTK_GRID(grid), btn_hello, 0, 0, 1, 1);
+
+	GtkWidget *btn_quit = gtk_button_new_with_label("Quit");
+	g_signal_connect(btn_quit, "clicked", G_CALLBACK(cb_quit), NULL);
+	gtk_grid_attach(GTK_GRID(grid), btn_quit, 0, 1, 1, 1);
+
+	GtkWidget *text_view = gtk_text_view_new();
+	GtkTextBuffer *text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+	gtk_text_buffer_set_text(text_buffer, "Hello world", -1);
+	gtk_grid_attach(GTK_GRID(grid), text_view, 0, 1, 2, 1);
+
+	// mostrar a janela
+	gtk_window_present(GTK_WINDOW(window));
+}
+
+static void cb_print_hello(GtkWidget *wid, gpointer data) {
+	g_print("Hello World\n");
+}
+
+static void cb_quit(GtkWindow *win) {
+	gtk_window_close(win);
 }
